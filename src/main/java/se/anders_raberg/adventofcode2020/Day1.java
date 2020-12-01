5e76aec94ca310dd49cbb54ae3fa0650ca07ac74
Anders Råberg <anders.raberg@gmail.com>
2020-12-01 21:16:52 +0100

    Day 1

--- src/main/java/se/anders_raberg/adventofcode2020/Day1.java
+++ src/main/java/se/anders_raberg/adventofcode2020/Day1.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 
 public class Day1 {
     private static final Logger LOGGER = Logger.getLogger(Day1.class.getName());
+    private static final Integer SUM = 2020;
 
     private Day1() {
     }
@@ -17,7 +18,20 @@ public class Day1 {
         List<Integer> data = Files.readAllLines(Paths.get("inputs/input1.txt")).stream() //
                 .map(Integer::valueOf) //
                 .collect(Collectors.toList());
-        
-        LOGGER.info(() -> String.format("Data: %s", data));
+
+        for (int i = 0; i < data.size(); i++) {
+            for (int j = i; j < data.size(); j++) {
+                if (SUM.equals(data.get(i) + data.get(j))) {
+                    LOGGER.info("Part 1: " + data.get(i) * data.get(j));
+                }
+
+                for (int k = j; k < data.size(); k++) {
+                    if (SUM.equals(data.get(i) + data.get(j) + data.get(k))) {
+                        LOGGER.info("Part 2: " + data.get(i) * data.get(j) * data.get(k));
+                    }
+
+                }
+            }
+        }
     }
 }
