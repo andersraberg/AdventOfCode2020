1ba3169ff29602dfcb54e602360d6d7e851e3ffd
Anders Råberg <anders.raberg@gmail.com>
2020-12-04 23:13:39 +0100

    Day 4, changed switch statement to map of lambdas

--- src/main/java/se/anders_raberg/adventofcode2020/Day4.java
+++ src/main/java/se/anders_raberg/adventofcode2020/Day4.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Map;
 import java.util.Set;
+import java.util.function.Predicate;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -17,34 +19,14 @@ public class Day4 {
     private enum PassportField {
         BYR, IYR, EYR, HGT, HCL, ECL, PID;
 
-        private static boolean valid(PassportField pField, String data) {
-            switch (pField) {
-            case BYR:
-                return inRange(Integer.parseInt(data), 1920, 2002);
-            case ECL:
-                return EYE_COLOR.contains(data);
-            case EYR:
-                return inRange(Integer.parseInt(data), 2020, 2030);
-            case HCL:
-                return data.matches("#[0-9,a-f]{6}");
-            case HGT:
-                Matcher m = HGT_PATTERN.matcher(data);
-                if (m.matches()) {
-                    int height = Integer.parseInt(m.group(1));
-                    if (m.group(2).equals("cm")) {
-                        return inRange(height, 150, 193);
-                    }
-                    return inRange(height, 59, 76);
-                }
-                return false;
-            case IYR:
-                return inRange(Integer.parseInt(data), 2010, 2020);
-            case PID:
-                return data.matches("[0-9]{9}");
-            default:
-                throw new IllegalArgumentException("Unexpected value: " + pField);
-            }
-        }
+        private static final Map<PassportField, Predicate<String>> VALIDITY_PREDICATES = Map.of( //
+                BYR, data -> inRange(Integer.parseInt(data), 1920, 2002), //
+                IYR, data -> inRange(Integer.parseInt(data), 2010, 2020), //
+                EYR, data -> inRange(Integer.parseInt(data), 2020, 2030), //
+                HGT, Day4::heightInRange, //
+                HCL, data -> data.matches("#[0-9,a-f]{6}"), //
+                ECL, EYE_COLOR::contains, //
+                PID, data -> data.matches("[0-9]{9}"));
     }
 
     private Day4() {
@@ -70,10 +52,23 @@ public class Day4 {
         return Arrays.stream(PassportField.values()).allMatch(f -> {
             Matcher m = Pattern.compile(f.toString().toLowerCase() + ":(\\S*)").matcher(passport);
             if (m.find()) {
-                return PassportField.valid(f, m.group(1));
+                return PassportField.VALIDITY_PREDICATES.get(f).test(m.group(1));
             }
             return false;
         });
+    }
+
+    private static boolean heightInRange(String data) {
+        Matcher m = HGT_PATTERN.matcher(data);
+        if (m.matches()) {
+            int height = Integer.parseInt(m.group(1));
+            if (m.group(2).equals("cm")) {
+                return inRange(height, 150, 193);
+            }
+            return inRange(height, 59, 76);
+        }
+        return false;
+
     }
 
     private static boolean inRange(int value, int min, int max) {
