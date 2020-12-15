0f0ae2b6dbe35ab6ea5d3eb1853597a386eda2ef
Anders Råberg <anders.raberg@gmail.com>
2020-12-15 21:23:21 +0100

    Day 14, cont'd

--- src/main/java/se/anders_raberg/adventofcode2020/DaysMain.java
+++ src/main/java/se/anders_raberg/adventofcode2020/DaysMain.java
@@ -4,20 +4,21 @@ import java.io.IOException;
 
 public class DaysMain {
     public static void main(String[] args) throws IOException {
-//        Day1.run();
-//        Day2.run();
-//        Day3.run();
-//        Day4.run();
-//        Day5.run();
-//        Day6.run();
-//        Day7.run();
-//        Day8.run();
-//        Day9.run();
-//        Day10.run();
-//        Day11.run();
-//        Day12Part1.run();
-//        Day12Part2.run();
-//        Day13.run();
+        Day1.run();
+        Day2.run();
+        Day3.run();
+        Day4.run();
+        Day5.run();
+        Day6.run();
+        Day7.run();
+        Day8.run();
+        Day9.run();
+        Day10.run();
+        Day11.run();
+        Day12Part1.run();
+        Day12Part2.run();
+        Day13.run();
+        Day14Part1.run();
         Day14Part2.run();
     }
 }
